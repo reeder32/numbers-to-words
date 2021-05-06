e364081608304ec565ed5111c43db0a9f5aa5e0b
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-06 14:21:27 -0700

    add failing test for converting number to word
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- NumbersToWords.Solution/NumbersToWords.Tests/NumbersToWordsTests.cs
+++ NumbersToWords.Solution/NumbersToWords.Tests/NumbersToWordsTests.cs
@@ -7,18 +7,42 @@ namespace NumbersToWords.Tests
   public class NumberToWordsTest
   {
     [TestMethod]
-
     public void ShouldReplaceOneWord_Hello_Universe()
     {
       string input = "Hello world";
       NumberConverter converter = new NumberConverter();
 
-      string result = converter.Convert(input, "world");
+      string result = converter.Convert(input, "world", "Universe");
 
       string output = "Hello Universe";
 
       Assert.AreEqual(result, output);
     }
 
+    [TestMethod]
+    public void ShouldReplaceCatWithDog_ConvertedString()
+    {
+      string input = "I am walking my cat to the cathedral";
+      NumberConverter converter = new NumberConverter();
+
+      string result = converter.Convert(input, "cat", "dog");
+
+      string output = "I am walking my dog to the doghedral";
+
+      Assert.AreEqual(result, output);
+    }
+
+    [TestMethod]
+    public void ShouldReplace1_One()
+    {
+      string input = "1";
+      NumberConverter converter = new NumberConverter();
+
+      string result = converter.ConvertNumberToWord(input);
+
+      string output = "one";
+
+      Assert.AreEqual(result, output);
+    }
   }
 }
